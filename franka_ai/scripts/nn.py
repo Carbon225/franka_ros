613e39b950ad56e2593f040b089cb19998b0f551
Carbon225 <carbon225@protonmail.com>
2025-10-29 11:28:21 +0100

    direct nn control

--- franka_ai/scripts/nn.py
+++ franka_ai/scripts/nn.py
@@ -2,6 +2,7 @@
 
 import rospy
 import rospkg
+from franka_msgs.msg import FrankaState
 
 import torch
 import torch.nn as nn
@@ -13,6 +14,11 @@ import pickle
 
 CTRL_DIM = 7
 OBS_DIM = 16
+
+
+agent = None
+obs_mean = None
+obs_std = None
 
 
 class Agent(nn.Module):
@@ -28,6 +34,9 @@ class Agent(nn.Module):
 
 
 def main():
+    global agent
+    global obs_mean, obs_std
+
     rospy.init_node('nn')
 
     pkg = 'franka_ai'
@@ -38,13 +47,38 @@ def main():
     rospy.loginfo('Loading model: %s', tensors_file)
     agent = Agent()
     load_model(agent, tensors_file, strict=False)
+    agent.eval()
     rospy.loginfo('Loading mean_std: %s', mean_std_file)
     with open(mean_std_file, 'rb') as f:
         obs_mean, obs_std = pickle.load(f)
     rospy.loginfo('NN loaded successfully')
 
+    rospy.Subscriber('franka_state_controller/franka_states', FrankaState, robot_state_callback)
+
     rospy.spin()
 
+
+def robot_state_callback(msg):
+    joint_positions = ...
+    joint_velocities = ...
+    phase_sin = ...
+    phase_cos = ...
+
+    obs = np.concatenate([
+        joint_positions,
+        joint_velocities,
+        phase_sin,
+        phase_cos,
+    ])
+
+    obs = (obs - obs_mean) / obs_std
+
+    with torch.inference_mode():
+        ctrl = agent.actor_mean(torch.Tensor(obs[np.newaxis, :])).numpy()[0]
+
+    position_setpoints = joint_positions + ctrl.clip(np.radians(-10), np.radians(10))
+
+    position_controller_pub.publish(setpoint_msg)
 
 if __name__ == '__main__':
     main()
